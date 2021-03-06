6e367085a22ae3945ebaac7fce3e8986d5592702
isisfa <isisfigueiredo@MacBook-Pro.Home>
2021-03-06 22:17:54 +0000

    function transaction

--- devFinances/script.js
+++ devFinances/script.js
@@ -7,3 +7,38 @@ const Modal = {
         document.querySelector('.modal-overlay').classList.remove('active');
     }
 }
+
+//table transactions
+const transactions = [
+    {
+    id: 1,
+    description: 'Energy',
+    amount: -50000,
+    date: '23/01/2021',
+},
+    {
+    id: 1,
+    description: 'Website',
+    amount: -500000,
+    date: '23/01/2021',
+},
+    {
+    id: 1,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+},
+]
+
+//balance
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses() {
+        //somar as saídas
+    },
+    total() {
+        //entradas - saídas
+    }
+}
